12cb4ffb78b85c9f2c79cb44082ba4840a60e40d
ankpandey1 <ankpandey1@gmail.com>
2020-02-27 19:04:06 +0100

    Initial commit. Will edit the files later

---
+++ guest-view.js
@@ -0,0 +1,12 @@
+function orderPlace(){
+	window.location.assign("orderPage.html");
+}
+
+
+function orderHistory(){
+	window.location.assign("orderHistory.html");
+}
+
+function pay(){
+	window.location.assign("paymentPage.html");
+}
