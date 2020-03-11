b8d5fe7998adbe913a7b2d58688e5da0580a122e
Nafi Zaman <zamannafi3@gmail.com>
2020-03-11 22:20:06 +0100

    Started working on drag and drop

---
+++ Dutchman Files/main.js
@@ -0,0 +1,110 @@
+var productsShown = false;
+
+$(function ()
+{
+    // Show main menu from the start. Hide all tables
+    //
+    $("#menuchoice").show();
+    $("#productspage").hide();
+    // if (!productsShown) displayProducts();
+
+    $("#productbutton").click(function ()
+    {
+        $("#menuchoice").hide("slow");
+        $("#productspage").show("slow");
+        if (!productsShown) displayProducts();
+    });
+
+    $("#tablesback").click(function ()
+    {
+        $("#menuchoice").show("slow");
+        $("#productspage").hide("slow");
+    });
+});
+
+function displayProducts()
+{
+    var DB = Products;
+
+    // console.log(DB);
+    var buttonCount = 0;
+
+    for (i = 0; i < Object.keys(DB).length; i++)
+    {
+        var type = Object.keys(DB)[i];
+        // console.log(DB[type].length);
+
+        for (j = 0; j < DB[type].length; j++)
+        {
+            var newDiv = document.createElement("div");
+            newDiv.className = "draggablediv";
+            var list = document.createElement("li");
+            var drinkName = document.createTextNode(DB[type][j].name);
+            var button = document.createElement("button");
+            button.id = buttonCount++;
+            button.innerHTML = "Show More";
+            button.setAttribute('onclick', "hideDiv(this.id)")
+            button.setAttribute('style', 'position:absolute; left:250px; border-bottom:groove');
+            newDiv.setAttribute('style', 'width:500px');
+            newDiv.setAttribute('style', 'margin-bottom:10px');
+
+            var drinkInfoDiv = document.createElement("div");
+            drinkInfoDiv.id = "drink" + button.id;
+            drinkInfoDiv.style = "display:none;border-style:solid;";
+
+            var jsonDataPretty = document.createElement("pre");
+
+            var allDrinkInfo = document.createTextNode(JSON.stringify(DB[type][j], null, 2));
+            var priceLabel = document.createElement("label");
+            priceLabel.innerHTML = "Price: ";
+
+            var priceBox = document.createElement("input");
+            priceBox.setAttribute('type', "number");
+            priceBox.setAttribute('value', DB[type][j].priceinclvat);
+
+            var changeDataButton = document.createElement("button");
+            changeDataButton.innerHTML = "Save Changes";
+            changeDataButton.setAttribute('style', 'border-bottom:groove');
+
+
+            list.appendChild(drinkName);
+            list.appendChild(button);
+            newDiv.appendChild(list);
+            newDiv.appendChild(drinkInfoDiv);
+            jsonDataPretty.appendChild(allDrinkInfo);
+            drinkInfoDiv.appendChild(jsonDataPretty);
+            drinkInfoDiv.appendChild(priceLabel);
+            drinkInfoDiv.appendChild(priceBox);
+            drinkInfoDiv.appendChild(changeDataButton);
+            newDiv.appendChild(drinkInfoDiv);
+
+            if (type == "beer") document.getElementById("beerlist").appendChild(newDiv);
+            else if (type == "wine") document.getElementById("winelist").appendChild(newDiv);
+            else if (type == "cocktail") document.getElementById("cocktaillist").appendChild(newDiv);
+            else if (type == "dishes") document.getElementById("dishlist").appendChild(newDiv);
+        }
+    }
+
+    // console.log(Object.keys(DB));
+
+
+    // console.log(DB);
+
+    productsShown = true;
+}
+
+function hideDiv(id)
+{
+    var x = document.getElementById("drink" + id);
+
+    if (x.style.display === "none")
+    {
+        $("#drink" + id).show("slow");
+        document.getElementById(id).innerHTML = "Show Less"
+    }
+    else
+    {
+        $("#drink" + id).hide("slow");
+        document.getElementById(id).innerHTML = "Show More"
+    }
+}
--- Dutchman Files/test.js
+++ Dutchman Files/test.js
@@ -1,109 +1,65 @@
-var productsShown = false;
+var fill = document.querySelector('.fill');
+var empties = document.querySelectorAll('.empty');
+var dragables = document.querySelector('.dragablediv');
 
-$(function ()
+//Fill listeners
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+dragables.addEventListener('dragstart', dragStart);
+dragables.addEventListener('dragend', dragEnd);
+
+//Loop through empties and call drag events
+
+for (const empty of empties)
 {
-    // Show main menu from the start. Hide all tables
-    //
-    $("#menuchoice").show();
-    $("#productspage").hide();
-    // displayProducts();
-
-    $("#productbutton").click(function ()
-    {
-        $("#menuchoice").hide("slow");
-        $("#productspage").show("slow");
-        if (!productsShown) displayProducts();
-    });
-
-    $("#tablesback").click(function ()
-    {
-        $("#menuchoice").show("slow");
-        $("#productspage").hide("slow");
-    });
-});
-
-function displayProducts()
-{
-    var DB = Products;
-
-    // console.log(DB);
-    var buttonCount = 0;
-
-    for (i = 0; i < Object.keys(DB).length; i++)
-    {
-        var type = Object.keys(DB)[i];
-        // console.log(DB[type].length);
-
-        for (j = 0; j < DB[type].length; j++)
-        {
-            var newDiv = document.createElement("div");
-            var list = document.createElement("li");
-            var drinkName = document.createTextNode(DB[type][j].name);
-            var button = document.createElement("button");
-            button.id = buttonCount++;
-            button.innerHTML = "Show More";
-            button.setAttribute('onclick', "hideDiv(this.id)")
-            button.setAttribute('style', 'position:absolute; left:250px; border-bottom:groove');
-            newDiv.setAttribute('style', 'width:500px');
-            newDiv.setAttribute('style', 'margin-bottom:10px');
-
-            var drinkInfoDiv = document.createElement("div");
-            drinkInfoDiv.id = "drink" + button.id;
-            drinkInfoDiv.style = "display:none;border-style:solid;";
-
-            var jsonDataPretty = document.createElement("pre");
-
-            var allDrinkInfo = document.createTextNode(JSON.stringify(DB[type][j], null, 2));
-            var priceLabel = document.createElement("label");
-            priceLabel.innerHTML = "Price: ";
-
-            var priceBox = document.createElement("input");
-            priceBox.setAttribute('type', "number");
-            priceBox.setAttribute('value', DB[type][j].priceinclvat);
-
-            var changeDataButton = document.createElement("button");
-            changeDataButton.innerHTML = "Save Changes";
-            changeDataButton.setAttribute('style', 'border-bottom:groove');
-
-
-            list.appendChild(drinkName);
-            list.appendChild(button);
-            newDiv.appendChild(list);
-            newDiv.appendChild(drinkInfoDiv);
-            jsonDataPretty.appendChild(allDrinkInfo);
-            drinkInfoDiv.appendChild(jsonDataPretty);
-            drinkInfoDiv.appendChild(priceLabel);
-            drinkInfoDiv.appendChild(priceBox);
-            drinkInfoDiv.appendChild(changeDataButton);
-            newDiv.appendChild(drinkInfoDiv);
-
-            if (type == "beer") document.getElementById("beerlist").appendChild(newDiv);
-            else if (type == "wine") document.getElementById("winelist").appendChild(newDiv);
-            else if (type == "cocktail") document.getElementById("cocktaillist").appendChild(newDiv);
-            else if (type == "dishes") document.getElementById("dishlist").appendChild(newDiv);
-        }
-    }
-
-    // console.log(Object.keys(DB));
-
-
-    // console.log(DB);
-
-    productsShown = true;
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
 }
 
-function hideDiv(id)
+// Drag functions
+function dragStart()
 {
-    var x = document.getElementById("drink" + id);
+    this.className += ' hold';
 
-    if (x.style.display === "none")
-    {
-        $("#drink" + id).show("slow");
-        document.getElementById(id).innerHTML = "Show Less"
-    }
-    else
-    {
-        $("#drink" + id).hide("slow");
-        document.getElementById(id).innerHTML = "Show More"
-    }
+    // setTimeout(function foo()
+    // {
+    //     if (this.className == 'fill hold')
+    //     {
+    //         this.className = 'invisible';
+    //         console.log("asd");
+    //     }
+    // }, 0);
+}
+
+function dragEnd()
+{
+    // console.log("This is when ending: ", this.className);
+    if (this.className == 'fill hold') this.className = 'fill';
+    else this.className = 'dragablediv';
+    // console.log(this.className);
+}
+
+function dragOver(e)
+{
+    e.preventDefault();
+    console.log(this.className);
+}
+
+function dragEnter(e)
+{
+    e.preventDefault();
+    this.className += ' hovered';
+}
+
+function dragLeave()
+{
+    this.className = 'empty';
+}
+
+function dragDrop()
+{
+    this.className = 'empty';
+    this.append(fill);
 }
